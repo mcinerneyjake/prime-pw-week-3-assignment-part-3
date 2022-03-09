5ec6a397d73592b27efee06d3cb59150b50bd33e
Jake McInerney <jakemcinerney1@gmail.com>
2022-03-08 21:51:22 -0600

    changed all quotes to single quotation marks

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,34 +1,34 @@
-console.log("****** Part Supply *******");
+console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
-console.log("1. Number of partsNeeded:");
+console.log('1. Number of partsNeeded:');
 
 let partsNeeded = 40;
 console.log(partsNeeded); // DONE
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log("2. Array of supplyChanges:");
+console.log('2. Array of supplyChanges:');
 
 supplyChanges = [3, 5, -6, 0, 7, 11];
 console.log(supplyChanges); // DONE
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log("3. Second supplyChange is:");
+console.log('3. Second supplyChange is:');
 
 console.log(supplyChanges[1]); // DONE
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
-console.log("4. Removed item:");
+console.log('4. Removed item:');
 
 let removedValue = supplyChanges.pop();
 console.log(removedValue); // DONE
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log("5. Adding 25 to supplyChanges.");
+console.log('5. Adding 25 to supplyChanges.');
 
 supplyChanges.push(25);
 console.log(supplyChanges); // DONE
@@ -38,7 +38,7 @@ console.log(supplyChanges); // DONE
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
-console.log("6. Showing supplyChanges...");
+console.log('6. Showing supplyChanges...');
 
 // THE FOLLOWING FUNCTION AND FOR LOOP WERE KEPT IN THE CODE SO THAT I COULD REMEMBER MY THOUGHT PROCESS WHILE SOLVING PROBLEM #6.
 // AND IT HELPS TO SOLIDIFY MY UNDERSTANDING OF THE .sort() AND .slice() FUNCTIONS. 😁
@@ -75,7 +75,7 @@ for (i = 0; i < supplyChanges.length; i++) {
 } // DONE
 
 // STRETCH GOALS
-console.log("---  Stretch Goals  ---");
+console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
@@ -91,7 +91,7 @@ for (supply of supplyChanges) {
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log("8. Total supplies available is:");
+console.log('8. Total supplies available is:');
 
 const sumOfSupplyChanges = supplyChanges.reduce(
   (partialSum, a) => partialSum + a,
